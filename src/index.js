6ee9f06a098451509a2b55f65b28fed89fdc8893
Marvel Mathew <m@marvelmathew.ca>
2017-04-15 17:59:49 -0400

    Archive URLs in received messages

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const erised = require('erised')
+const TelegramBot = require('node-telegram-bot-api')
+
+const token = process.env.TELEGRAM_BOT_TOKEN
+const bot = new TelegramBot(token, {polling: true})
+
+function extractUrls (entities, text) {
+  return entities
+    .filter(e => e.type === 'url')
+    .map(({offset, length}) => text.slice(offset, offset + length))
+}
+
+bot.on('message', msg => {
+  const {entities, text} = msg
+  console.log(JSON.stringify(msg, null, 2))
+
+  if (entities) {
+    const urls = new Set(extractUrls(entities, text))
+    urls.forEach(url => {
+      erised.archivePage(url, 'bot')
+    })
+  }
+})
